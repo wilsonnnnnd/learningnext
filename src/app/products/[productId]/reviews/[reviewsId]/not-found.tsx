3919189beaf261d7b30597a380e54517b74ec21d
Wilson <wilson.ding.wm@gmail.com>
2024-05-22 00:52:27 +1000

    Page not found

---
+++ src/app/products/[productId]/reviews/[reviewsId]/not-found.tsx
@@ -0,0 +1,8 @@
+    export default function NotFound(){
+        return (
+            <div>
+                <h1>Page Not Found</h1>
+                <h2>Could not find resource</h2>
+            </div>
+        )
+    }
--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,10 +1,18 @@
+import { notFound } from "Next/navigation";
 export default function ReviewProductDetails({
     params,
 }: {
-    params: { 
-        productId: string 
-        reviewsId : string
+    params: {
+        productId: string;
+        reviewsId: string;
     };
 }) {
-    return <h1>Review {params.reviewsId} for Product  Detail {params.productId}</h1>;
+    if (parseInt(params.reviewsId)> 1000) {
+        notFound();
+    }
+    return (
+        <h1>
+            Review {params.reviewsId} for Product Detail {params.productId}
+        </h1>
+    );
 }
